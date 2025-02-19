6cdfca3c3d87b869abf3a8652d7e09e887d3727e
jhon3242 <rldotnr2@gmail.com>
2025-02-19 12:21:19 +0900

    chore: 개행 삭제

---
+++ src/main/kotlin/com/deepromeet/seulseul/common/exception/CustomException.kt
@@ -0,0 +1,13 @@
+package com.deepromeet.seulseul.common.exception
+
+abstract class CustomException(
+    val errorType: BaseErrorType,
+    private val sourceLayer: String,
+) : RuntimeException() {
+
+    val status: Int
+        get() = errorType.errorReason.status
+
+    override val message: String
+        get() = sourceLayer.let { "$it - ${errorType.errorReason.message}" }
+}
