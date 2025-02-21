243bdba734b38765507bab9d2ba6c028abaf8ecf
jhon3242 <rldotnr2@gmail.com>
2025-02-21 17:02:44 +0900

    chore: 불필요한 코드 삭제
    토큰 정보 조회 로직 삭제

--- src/main/kotlin/com/deepromeet/seulseul/auth/infrastructure/client/KakaoApiClient.kt
+++ src/main/kotlin/com/deepromeet/seulseul/auth/infrastructure/client/KakaoApiClient.kt
@@ -1,7 +1,6 @@
 package com.deepromeet.seulseul.auth.infrastructure.client
 
 import com.deepromeet.seulseul.auth.api.controller.LoggingConfiguration
-import com.deepromeet.seulseul.auth.infrastructure.response.KaKaoOAuthTokenInfoResponse
 import com.deepromeet.seulseul.auth.infrastructure.response.KakaoUserInfoResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,9 +12,6 @@ import org.springframework.web.bind.annotation.RequestHeader
     configuration = [kakaoFeignConfig::class, LoggingConfiguration::class]
 )
 interface KakaoApiClient {
-    @GetMapping("/v1/user/access_token_info")
-    fun getTokenInfo(@RequestHeader("Authorization") accessToken : String) : KaKaoOAuthTokenInfoResponse
-
     @GetMapping("/v2/user/me")
     fun getUserInfo(@RequestHeader("Authorization") accessToken : String) : KakaoUserInfoResponse
 
