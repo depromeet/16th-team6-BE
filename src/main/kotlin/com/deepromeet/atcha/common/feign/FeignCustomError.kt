58342e08e26ea095dd1aa1460a386daa634064f0
Jiwon <rkdwldnjs878@gmail.com>
2025-02-24 06:51:01 +0900

    feat: feignCustomError 403 추가

--- src/main/kotlin/com/deepromeet/atcha/common/feign/FeignCustomError.kt
+++ src/main/kotlin/com/deepromeet/atcha/common/feign/FeignCustomError.kt
@@ -11,6 +11,7 @@ enum class FeignErrorType(
     override val logLevel: LogLevel
 ) : BaseErrorType {
     EXTERNAL_API_BAD_REQUEST_ERROR(400, "EXT_002", "외부 API 요청이 잘못되었습니다", LogLevel.WARN),
+    EXTERNAL_API_FORBIDDEN_ERROR(403, "EXT_004", "외부 API에 접근할 권한이 없습니다", LogLevel.WARN),
     EXTERNAL_API_NOT_FOUND_ERROR(404, "EXT_001", "외부 API에서 데이터를 찾을 수 없습니다", LogLevel.WARN),
     EXTERNAL_API_INTERNAL_SERVER_ERROR(500, "EXT_003", "외부 API 서버에서 오류가 발생했습니다", LogLevel.ERROR)
 }
@@ -20,6 +21,10 @@ sealed class FeignException(
 ) : CustomException(errorCode) {
     data object ExternalApiNotFoundError : FeignException(FeignErrorType.EXTERNAL_API_NOT_FOUND_ERROR) {
         override fun readResolve(): Any = ExternalApiNotFoundError
+    }
+
+    data object ExternalApiForbiddenError : FeignException(FeignErrorType.EXTERNAL_API_FORBIDDEN_ERROR) {
+        override fun readResolve(): Any = ExternalApiForbiddenError
     }
 
     data object ExternalApiBadRequestError : FeignException(FeignErrorType.EXTERNAL_API_BAD_REQUEST_ERROR) {
