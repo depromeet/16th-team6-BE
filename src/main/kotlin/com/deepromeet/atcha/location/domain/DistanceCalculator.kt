f150dbc48b2db840eb39bff8825afa3f277fdb29
Jiwon <rkdwldnjs878@gmail.com>
2025-02-25 05:04:58 +0900

    refactor: 유틸 클래스를 object로 변경

--- src/main/kotlin/com/deepromeet/atcha/location/domain/DistanceCalculator.kt
+++ src/main/kotlin/com/deepromeet/atcha/location/domain/DistanceCalculator.kt
@@ -6,37 +6,33 @@ import kotlin.math.pow
 import kotlin.math.sin
 import kotlin.math.sqrt
 
-class DistanceCalculator {
-    companion object {
-        private const val EARTH_RADIUS_KM = 6371.0 // 지구 반지름 (km)
+object DistanceCalculator {
+    private const val EARTH_RADIUS_KM = 6371.0 // 지구 반지름 (km)
 
-        fun calculate(
-            pois: List<POI>,
-            currentCoordinate: Coordinate
-        ): List<POI> {
-            return pois.map { poi ->
-                poi.copy(radius = haversine(poi.location.coordinate, currentCoordinate))
-            }
+    fun calculate(
+        pois: List<POI>,
+        currentCoordinate: Coordinate
+    ): List<POI> {
+        return pois.map { poi ->
+            poi.copy(radius = haversine(poi.location.coordinate, currentCoordinate))
         }
+    }
 
-        fun haversine(
-            targetCoordinate: Coordinate,
-            currentCoordinate: Coordinate
-        ): Int {
-            with(targetCoordinate) {
-                val latDistance = Math.toRadians(lat - currentCoordinate.lat)
-                val lonDistance = Math.toRadians(lon - currentCoordinate.lon)
+    private fun haversine(
+        targetCoordinate: Coordinate,
+        currentCoordinate: Coordinate
+    ): Int {
+        val latDistance = Math.toRadians(targetCoordinate.lat - currentCoordinate.lat)
+        val lonDistance = Math.toRadians(targetCoordinate.lon - currentCoordinate.lon)
 
-                val a =
-                    sin(latDistance / 2).pow(2) +
-                        cos(Math.toRadians(currentCoordinate.lat)) * cos(Math.toRadians(lat)) *
-                        sin(lonDistance / 2).pow(2)
+        val a =
+            sin(latDistance / 2).pow(2) +
+                cos(Math.toRadians(currentCoordinate.lat)) * cos(Math.toRadians(targetCoordinate.lat)) *
+                sin(lonDistance / 2).pow(2)
 
-                val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-                val distanceKm = EARTH_RADIUS_KM * c // 거리 (km)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        val distanceKm = EARTH_RADIUS_KM * c // 거리 (km)
 
-                return distanceKm.toInt() // Int로 변환하여 반환
-            }
-        }
+        return distanceKm.toInt() // Int로 변환하여 반환
     }
 }
