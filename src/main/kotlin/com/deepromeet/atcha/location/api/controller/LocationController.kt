dc664075428fee5fb3ac5c897fbff0df5f856197
Jiwon <rkdwldnjs878@gmail.com>
2025-02-24 22:09:39 +0900

    refactor: RequestBody 제거
    - Get Method에서는 Body를 사용하지않음

--- src/main/kotlin/com/deepromeet/atcha/location/api/controller/LocationController.kt
+++ src/main/kotlin/com/deepromeet/atcha/location/api/controller/LocationController.kt
@@ -5,11 +5,9 @@ import com.deepromeet.atcha.common.web.ApiResponse
 import com.deepromeet.atcha.common.web.SliceResponse
 import com.deepromeet.atcha.location.api.request.LocationSearchRequest
 import com.deepromeet.atcha.location.api.response.LocationResponse
-import com.deepromeet.atcha.location.api.response.LocationSearchResponse
 import com.deepromeet.atcha.location.domain.LocationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,16 +18,16 @@ class LocationController(
 ) {
     @GetMapping
     fun getLocations(
-        @RequestBody request: LocationSearchRequest,
+        @ModelAttribute request: LocationSearchRequest,
         @ModelAttribute cursor: Cursor
-    ): ApiResponse<LocationSearchResponse> =
+    ): ApiResponse<SliceResponse<LocationResponse>> =
         locationService.getLocations(
             request.keyword,
             request.toCoordinate(),
             cursor
         ).let {
             return ApiResponse.success(
-                LocationSearchResponse(SliceResponse.from(it.map { location -> LocationResponse.from(location) }))
+                SliceResponse.from(it.map { location -> LocationResponse.from(location) })
             )
         }
 }
