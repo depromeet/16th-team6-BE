1c8ff1370eda77dd3d5bd16c9e374511d487292b
jhon3242 <rldotnr2@gmail.com>
2025-02-24 18:53:10 +0900

    chore: kakaoFeignConfig 이름 변경

--- src/main/kotlin/com/deepromeet/seulseul/auth/infrastructure/client/KakaoFeignConfig.kt
+++ src/main/kotlin/com/deepromeet/seulseul/auth/infrastructure/client/KakaoFeignConfig.kt
@@ -3,7 +3,7 @@ package com.deepromeet.seulseul.auth.infrastructure.client
 import feign.RequestInterceptor
 import org.springframework.context.annotation.Bean
 
-class kakaoFeignConfig {
+class KakaoFeignConfig {
     @Bean
     fun requestInterceptor() : RequestInterceptor {
         return RequestInterceptor { requestTemplate ->
