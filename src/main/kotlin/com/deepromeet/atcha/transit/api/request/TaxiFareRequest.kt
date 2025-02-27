fe0002134f9fdf7f2e3836a416f55c2607d31ebe
Jiwon <rkdwldnjs878@gmail.com>
2025-02-27 18:56:12 +0900

    feat: 요청값 검증 추가

--- src/main/kotlin/com/deepromeet/atcha/transit/api/request/TaxiFareRequest.kt
+++ src/main/kotlin/com/deepromeet/atcha/transit/api/request/TaxiFareRequest.kt
@@ -8,7 +8,14 @@ data class TaxiFareRequest(
     val endLat: Double,
     val endLon: Double
 ) {
-    fun toOrigin(): Coordinate = Coordinate(startLat, startLon)
+    init {
+        require(startLat in -90.0..90.0) { "잘못된 위도 값입니다: $startLat (유효 범위: -90.0 ~ 90.0)" }
+        require(startLon in -180.0..180.0) { "잘못된 경도 값입니다: $startLon (유효 범위: -180.0 ~ 180.0)" }
+        require(endLat in -90.0..90.0) { "잘못된 위도 값입니다: $endLat (유효 범위: -90.0 ~ 90.0)" }
+        require(endLon in -180.0..180.0) { "잘못된 경도 값입니다: $endLon (유효 범위: -180.0 ~ 180.0)" }
+    }
 
-    fun toDestination(): Coordinate = Coordinate(endLat, endLon)
+    fun toStart(): Coordinate = Coordinate(startLat, startLon)
+
+    fun toEnd(): Coordinate = Coordinate(endLat, endLon)
 }
