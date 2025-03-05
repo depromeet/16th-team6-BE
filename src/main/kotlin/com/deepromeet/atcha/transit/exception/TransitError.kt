c4299da4aed91e8c18dfad44fcc1b31d4b2c5540
Jiwon <rkdwldnjs878@gmail.com>
2025-03-06 08:24:55 +0900

    feat: 에러코드 추가

--- src/main/kotlin/com/deepromeet/atcha/transit/exception/TransitError.kt
+++ src/main/kotlin/com/deepromeet/atcha/transit/exception/TransitError.kt
@@ -14,7 +14,10 @@ enum class TransitErrorType(
     TAXI_FARE_FETCH_FAILED(500, "TRS_002", "택시 요금 조회에 실패했습니다", LogLevel.ERROR),
     INVALID_BUS_ROUTE(400, "TRS_003", "유효하지 않은 버스 노선입니다", LogLevel.ERROR),
     INVALID_BUS_STATION(400, "TRS_004", "유효하지 않은 버스 정류장입니다", LogLevel.ERROR),
-    NOT_FOUND_BUS_ARRIVAL(404, "TRS_005", "버스 도착 정보를 찾을 수 없습니다", LogLevel.ERROR)
+    NOT_FOUND_BUS_ARRIVAL(404, "TRS_005", "버스 도착 정보를 찾을 수 없습니다", LogLevel.ERROR),
+    NOT_FOUND_SUBWAY_STATION(404, "TRS_006", "지하철 역을 찾을 수 없습니다", LogLevel.ERROR),
+    NOT_FOUND_SUBWAY_TIME_TABLE(404, "TRS_007", "지하철 시간표를 찾을 수 없습니다", LogLevel.ERROR),
+    SUBWAY_DIRECTION_RESOLVE_FAILED(400, "TRS_008", "유효하지 않은 지하철 방향입니다", LogLevel.ERROR)
 }
 
 sealed class TransitException(
@@ -34,5 +37,17 @@ sealed class TransitException(
 
     data object NotFoundBusArrival : TransitException(TransitErrorType.NOT_FOUND_BUS_ARRIVAL) {
         override fun readResolve(): Any = NotFoundBusArrival
+    }
+
+    data object NotFoundSubwayStation : TransitException(TransitErrorType.NOT_FOUND_SUBWAY_STATION) {
+        override fun readResolve(): Any = NotFoundSubwayStation
+    }
+
+    data object NotFoundSubwayTimeTable : TransitException(TransitErrorType.NOT_FOUND_SUBWAY_TIME_TABLE) {
+        override fun readResolve(): Any = NotFoundSubwayTimeTable
+    }
+
+    data object SubwayDirectionResolveFailed : TransitException(TransitErrorType.SUBWAY_DIRECTION_RESOLVE_FAILED) {
+        override fun readResolve(): Any = SubwayDirectionResolveFailed
     }
 }
