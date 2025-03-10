5a58fd862d99df84188018d20428eecf13a692b4
jhon3242 <rldotnr2@gmail.com>
2025-03-10 10:23:04 +0900

    feat: deploy에 nginx 시작 작업 추가

--- deployment/dev/deploy.sh
+++ deployment/dev/deploy.sh
@@ -12,37 +12,48 @@ check_service() {
 
   # 최대 재시도 횟수
   while [ $RETRIES -lt $MAX_RETRIES ]; do
-    echo "Checking service at $SERVICE_NAME (attempt: $((RETRIES+1)))"
+    echo "Checking service $SERVICE_NAME (attempt: $((RETRIES+1)))"
     sleep 3
 
-    # 서비스에 대한 모든 컨테이너 ID 가져오기
     local all_healthy=true
 
     # 각 컨테이너의 헬스 상태 검사
     for id in "${container_ids[@]}"; do
-      local health_status=$(docker container inspect --format='{{.State.Health.Status}}' "$id")
+      local health_status
+      health_status=$(docker container inspect --format='{{.State.Health.Status}}' "$id")
       echo "Health status of container $id: $health_status"
-
       if [ "$health_status" != "healthy" ]; then
         all_healthy=false
         break
       fi
     done
 
-    # 모든 컨테이너가 healthy 상태일 경우
     if [ "$all_healthy" = true ]; then
       echo "$SERVICE_NAME health check passed."
       return 0
     fi
 
     RETRIES=$((RETRIES+1))
-  done;
+  done
 
   echo "Failed to check service $SERVICE_NAME after $MAX_RETRIES attempts."
   return 1
 }
 
-if [ -z "$IS_BLUE" ];then
+ensure_nginx_running() {
+  local nginx_exists
+  nginx_exists=$(docker compose ps -q nginx)
+  if [ -z "$nginx_exists" ]; then
+    echo "nginx 컨테이너가 존재하지 않습니다. nginx 컨테이너를 실행합니다."
+    docker compose up -d nginx
+    # nginx가 완전히 실행될 때까지 잠시 대기
+    sleep 5
+  else
+    echo "nginx 컨테이너가 이미 실행 중입니다."
+  fi
+}
+
+if [ -z "$IS_BLUE" ]; then
   echo "### GREEN => BLUE ###"
 
   echo "1. BLUE 이미지 받기"
@@ -51,17 +62,18 @@ if [ -z "$IS_BLUE" ];then
   echo "2. BLUE 컨테이너 실행"
   docker compose up -d atcha-blue
 
-  echo "3. health check"
+  echo "3. BLUE 컨테이너 헬스 체크"
   if ! check_service "atcha-blue"; then
     echo "BLUE health check failed."
     exit 1
   fi
 
   echo "4. nginx 재실행"
+  ensure_nginx_running
   sudo cp data/nginx/nginx-blue.conf data/nginx/nginx.conf
   sudo docker compose exec -it nginx nginx -s reload
 
-  echo "5. GREEN  컨테이너 내리기"
+  echo "5. GREEN 컨테이너 중지 및 삭제"
   docker compose stop atcha-green
   docker compose rm -f atcha-green
 
@@ -74,17 +86,18 @@ else
   echo "2. GREEN 컨테이너 실행"
   docker compose up -d atcha-green
 
-  echo "3. health check"
+  echo "3. GREEN 컨테이너 헬스 체크"
   if ! check_service "atcha-green"; then
     echo "GREEN health check failed."
     exit 1
   fi
 
   echo "4. nginx 재실행"
+  ensure_nginx_running
   sudo cp data/nginx/nginx-green.conf data/nginx/nginx.conf
   sudo docker compose exec -it nginx nginx -s reload
 
-  echo "5. BLUE 컨테이너 내리기"
+  echo "5. BLUE 컨테이너 중지 및 삭제"
   docker compose stop atcha-blue
   docker compose rm -f atcha-blue
 fi
