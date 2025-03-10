3096a52ed18a2f39094fc9fa570a50df5526a015
jhon3242 <rldotnr2@gmail.com>
2025-03-10 21:52:21 +0900

    chore: 엑세스 토큰 만료시간 짧게 수정(15초)

--- src/main/kotlin/com/deepromeet/atcha/common/token/TokenType.kt
+++ src/main/kotlin/com/deepromeet/atcha/common/token/TokenType.kt
@@ -4,6 +4,6 @@ enum class TokenType(
     val expirationMills: Long
 ) {
 //    ACCESS(1000L * 60 * 30), // 30분
-    ACCESS(1000L * 3), // 3초
+    ACCESS(1000L * 15), // 15초
     REFRESH(1000L * 60 * 60 * 24 * 60) // 60일
 }
