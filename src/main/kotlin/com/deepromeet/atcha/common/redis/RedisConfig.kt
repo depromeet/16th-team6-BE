3cc241e6f406bd4d24e39b217f6098dd9b52fecc
jhon3242 <rldotnr2@gmail.com>
2025-04-01 17:46:56 +0900

    feat: RedisConfig 비밀번호 추가

--- src/main/kotlin/com/deepromeet/atcha/common/redis/RedisConfig.kt
+++ src/main/kotlin/com/deepromeet/atcha/common/redis/RedisConfig.kt
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
@@ -23,11 +25,15 @@ class RedisConfig(
     @Value("\${redis.host}")
     private val host: String,
     @Value("\${redis.port}")
-    private val port: Int
+    private val port: Int,
+    @Value("\${redis.password}")
+    private val password: String
 ) {
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(host, port)
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration(host, port)
+        redisStandaloneConfiguration.password = RedisPassword.of(password)
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
     }
 
     @Bean
