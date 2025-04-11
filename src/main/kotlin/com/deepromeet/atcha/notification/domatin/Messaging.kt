8fc9176deb78fa8a1277501cd4726b3fdc3b7a14
jhon3242 <rldotnr2@gmail.com>
2025-04-11 15:39:12 +0900

    fix: Messaging 객체 생성 버그 수정

--- src/main/kotlin/com/deepromeet/atcha/notification/domatin/Messaging.kt
+++ src/main/kotlin/com/deepromeet/atcha/notification/domatin/Messaging.kt
@@ -7,9 +7,9 @@ data class Messaging(
     val dataMap: Map<String, String> = emptyMap()
 ) {
     constructor(notificationContent: NotificationContent, token: String) : this(
-        notificationContent.title,
-        notificationContent.body,
-        token,
-        notificationContent.dataMap
+        token = token,
+        title = notificationContent.title,
+        body = notificationContent.body,
+        dataMap = notificationContent.dataMap
     )
 }
