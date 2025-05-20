e0d1f216925de677c5707c2898133002a27b749e
junwon <wjdwnsdnjs13@naver.com>
2025-05-20 20:51:17 +0900

    feat : ODSay API 요청 클라이언트 추가

---
+++ src/main/kotlin/com/deepromeet/atcha/transit/infrastructure/client/odsay/ODSayBusFeignClient.kt
@@ -0,0 +1,37 @@
+package com.deepromeet.atcha.transit.infrastructure.client.odsay
+
+import com.deepromeet.atcha.transit.infrastructure.client.odsay.response.ODSayBusArrivalResponse
+import com.deepromeet.atcha.transit.infrastructure.client.odsay.response.ODSayBusStationInfoResponse
+import com.deepromeet.atcha.transit.infrastructure.client.public.config.PublicFeignConfig
+import com.deepromeet.atcha.transit.infrastructure.client.public.response.BusArrivalResponse
+import com.deepromeet.atcha.transit.infrastructure.client.public.response.ServiceResult
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@FeignClient(
+    name = "odsay-bus-arrival-info",
+    url = "\${odsay.api.url}",
+    configuration = [PublicFeignConfig::class]
+)
+interface ODSayBusFeignClient {
+    @GetMapping("/v1/api/busLaneDetail")
+    fun getArrivalInfoByRoute(
+        @RequestParam busID: String,
+        @RequestParam apiKey: String
+    ): ServiceResult<BusArrivalResponse>
+
+    @GetMapping("/v1/api/searchStation")
+    fun getStationByStationName(
+        @RequestParam apiKey: String,
+        @RequestParam stationName: String,
+        @RequestParam lang: String = "0"
+    ): ODSayBusArrivalResponse
+
+    @GetMapping("/v1/api/busStationInfo")
+    fun getStationInfoBystationID(
+        @RequestParam apiKey: String,
+        @RequestParam stationID: String,
+        @RequestParam lang: String = "0"
+    ): ODSayBusStationInfoResponse
+}
