d12ccd38cc7508e43b61eeffb8c5a2d51039c63d
jhon3242 <rldotnr2@gmail.com>
2025-07-07 12:19:28 +0900

    test: 유저 알림 조회 테스트 추가

--- src/test/kotlin/com/deepromeet/atcha/notification/NotificationServiceTest.kt
+++ src/test/kotlin/com/deepromeet/atcha/notification/NotificationServiceTest.kt
@@ -1,16 +1,21 @@
 package com.deepromeet.atcha.notification
 
 import com.deepromeet.atcha.notification.domatin.NotificationService
+import com.deepromeet.atcha.notification.domatin.UserNotificationReader
 import com.deepromeet.atcha.support.BaseServiceTest
 import com.deepromeet.atcha.support.fixture.LastRouteFixture
 import com.deepromeet.atcha.support.fixture.UserFixture
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDateTime
 
 class NotificationServiceTest : BaseServiceTest() {
     @Autowired
     private lateinit var notificationService: NotificationService
+
+    @Autowired
+    lateinit var notificationReader: UserNotificationReader
 
     @Test
     fun `경로를 알림으로 등록한다`() {
@@ -49,5 +54,33 @@ class NotificationServiceTest : BaseServiceTest() {
         // then
         val userNotifications = userNotificationReader.findById(user.id, lastRoute.routeId)
         Assertions.assertThat(userNotifications).hasSize(0)
+    }
+
+    @Test
+    fun `사용자의 알림 빈도만큼 알림을 추가한다`() {
+        // given
+        val maxMinute = 10
+        val user =
+            userAppender.append(
+                UserFixture.create(
+                    alertFrequencies = mutableSetOf(1, 5, maxMinute)
+                )
+            )
+        val lastRoute =
+            LastRouteFixture.create(
+                departureDateTime = LocalDateTime.now().plusMinutes(maxMinute.toLong() + 10).toString()
+            )
+        lastRouteAppender.append(lastRoute)
+        notificationService.addRouteNotification(user.id, lastRoute.routeId)
+
+        // when
+        val userNotifications =
+            notificationReader.findAll()
+                .filter { it.userId == user.id }
+
+        // then
+        Assertions.assertThat(userNotifications).hasSize(
+            user.alertFrequencies.size
+        )
     }
 }
