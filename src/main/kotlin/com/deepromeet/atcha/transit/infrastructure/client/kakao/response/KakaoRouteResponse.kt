d874cc9a6f48a7ceed7fa4db8b19ea02f8114ec1
Jiwon <rkdwldnjs878@gmail.com>
2025-07-07 21:32:20 +0900

    feat: 카카오 택시비 예외 추가

--- src/main/kotlin/com/deepromeet/atcha/transit/infrastructure/client/kakao/response/KakaoRouteResponse.kt
+++ src/main/kotlin/com/deepromeet/atcha/transit/infrastructure/client/kakao/response/KakaoRouteResponse.kt
@@ -1,11 +1,24 @@
 package com.deepromeet.atcha.transit.infrastructure.client.kakao.response
 
 import com.deepromeet.atcha.transit.domain.Fare
+import com.deepromeet.atcha.transit.exception.TransitError
+import com.deepromeet.atcha.transit.exception.TransitException
 
 data class KakaoRouteResponse(
-    val results: List<RouteResult>?
+    val results: List<RouteResult>?,
+    val status: KakaoStatus
 ) {
     fun firstTaxiFare(): Fare? {
+        if (status.code == "START_NOT_FOUND") {
+            throw TransitException.of(TransitError.TAXI_START_NOT_FOUND)
+        }
+
+        if (status.code != "SUCCESS") {
+            throw TransitException.of(
+                TransitError.TAXI_FARE_FETCH_FAILED,
+                status.message ?: "택시비 조회에 실패했습니다."
+            )
+        }
         return results?.firstOrNull()?.summary?.fare?.taxi?.let { Fare(it) }
     }
 }
@@ -20,4 +33,9 @@ data class RouteSummary(
 
 data class KakaoFare(
     val taxi: Int
+)
+
+data class KakaoStatus(
+    val code: String,
+    val message: String?
 )
