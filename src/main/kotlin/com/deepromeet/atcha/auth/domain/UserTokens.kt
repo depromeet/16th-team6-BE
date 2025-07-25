858db3f8f53e8d6f6bb515daad2b5310386491c3
Jiwon <rkdwldnjs878@gmail.com>
2025-07-25 22:15:26 +0900

    refactor: UserTokenInfo를 UserTokens로 이름 변경 #323
    - 파일명과 클래스명을 UserTokens로 통일
    - 더 명확하고 일관된 네이밍 적용

--- src/main/kotlin/com/deepromeet/atcha/auth/domain/UserTokens.kt
+++ src/main/kotlin/com/deepromeet/atcha/auth/domain/UserTokens.kt
@@ -3,7 +3,7 @@ package com.deepromeet.atcha.auth.domain
 import com.deepromeet.atcha.shared.web.token.TokenInfo
 import com.deepromeet.atcha.user.domain.UserId
 
-data class UserTokenInfo(
+data class UserTokens(
     val id: UserId,
     val accessToken: String,
     val refreshToken: String
