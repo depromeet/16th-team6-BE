1698e35a34abaccf454e855eb8c78eae1e79a786
Jiwon <rkdwldnjs878@gmail.com>
2025-07-26 05:54:53 +0900

    feat: UserUpdateInfo에 HomeAddress 생성 헬퍼 메서드 추가
    null 안전성을 위해 getHomeAddress() 메서드를 추가하여
    주소 정보가 완전할 때만 HomeAddress 객체를 생성하도록 개선

--- src/main/kotlin/com/deepromeet/atcha/user/domain/UserUpdateInfo.kt
+++ src/main/kotlin/com/deepromeet/atcha/user/domain/UserUpdateInfo.kt
@@ -1,5 +1,7 @@
 package com.deepromeet.atcha.user.domain
 
+import com.deepromeet.atcha.location.domain.Coordinate
+
 data class UserUpdateInfo(
     val nickname: String?,
     val profileImageUrl: String?,
@@ -8,4 +10,19 @@ data class UserUpdateInfo(
     val lon: Double?,
     val alertFrequencies: MutableSet<Int>?,
     val fcmToken: String?
-)
+) {
+    fun hasHomeAddress(): Boolean {
+        return address != null && lat != null && lon != null
+    }
+
+    fun getHomeAddress(): HomeAddress? {
+        return if (hasHomeAddress()) {
+            HomeAddress(
+                address = address!!,
+                coordinate = Coordinate(lat = lat!!, lon = lon!!)
+            )
+        } else {
+            null
+        }
+    }
+}
