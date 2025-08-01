6b77000d2d2a29f2ef9470c51303403bb74b71e8
Jiwon <rkdwldnjs878@gmail.com>
2025-08-01 18:08:53 +0900

    feat: SubwayInfo에 지하철 방면 정보 해결 메서드 추가 #333
    - 종착역 이름 반환 메서드 resolveFinalStationName 추가
    - 방면 정보 반환 메서드 resolveDirectionName 추가
    - 순환선 여부에 따른 방면명 처리 (상행/하행, 내선/외선)

--- src/main/kotlin/com/deepromeet/atcha/transit/domain/TransitInfo.kt
+++ src/main/kotlin/com/deepromeet/atcha/transit/domain/TransitInfo.kt
@@ -4,10 +4,20 @@ import com.deepromeet.atcha.transit.domain.bus.BusRoute
 import com.deepromeet.atcha.transit.domain.bus.BusSchedule
 import com.deepromeet.atcha.transit.domain.bus.BusStation
 import com.deepromeet.atcha.transit.domain.bus.BusTimeTable
+import com.deepromeet.atcha.transit.domain.subway.SubwayLine
+import com.deepromeet.atcha.transit.domain.subway.SubwayTime
 import com.deepromeet.atcha.transit.domain.subway.SubwayTimeTable
 
 sealed class TransitInfo {
-    data class SubwayInfo(val timeTable: SubwayTimeTable) : TransitInfo()
+    data class SubwayInfo(
+        val subwayLine: SubwayLine,
+        val timeTable: SubwayTimeTable,
+        val targetSchedule: SubwayTime
+    ) : TransitInfo() {
+        fun resolveFinalStationName(): String = targetSchedule.finalStation.name
+
+        fun resolveDirectionName(): String = targetSchedule.subwayDirection.getName(subwayLine.isCircular)
+    }
 
     data class BusInfo(
         val busRoute: BusRoute,
