4bdec0c97413305e8e8eef532673599ec5a6faaa
junwon <wjdwnsdnjs13@naver.com>
2025-08-12 23:39:47 +0900

    refactor : 믹스패널 프로퍼티 분리

---
+++ src/main/kotlin/com/deepromeet/atcha/miaxpanel/event/ODsayCallRouteProperty.kt
@@ -0,0 +1,7 @@
+package com.deepromeet.atcha.miaxpanel.event
+
+class ODsayCallRouteProperty(
+    val serviceRegion: String,
+    val startStationName: String,
+    val endStationName: String
+) : MixpanelEventProperty
