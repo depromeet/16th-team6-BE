863fd734d7f73c749010ef197d0e8804343f59da
junwon <wjdwnsdnjs13@naver.com>
2025-08-12 23:40:14 +0900

    refactor : 믹스패널 이벤트 네임 분리

--- src/main/kotlin/com/deepromeet/atcha/miaxpanel/MixpanelEvent.kt
+++ src/main/kotlin/com/deepromeet/atcha/miaxpanel/MixpanelEvent.kt
@@ -5,9 +5,5 @@ import com.deepromeet.atcha.miaxpanel.event.MixpanelEventProperty
 class MixpanelEvent(
     val mixpanelEventName: MixpanelEventName,
     val distinctId: String,
-    val property: MixpanelEventProperty,
-) {
-    enum class MixpanelEventName(val value: String) {
-        ODSAY_CALL_COUNT("odsay_call_count"),
-    }
-}
+    val property: MixpanelEventProperty
+)
