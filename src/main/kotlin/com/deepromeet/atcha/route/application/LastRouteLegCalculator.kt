9c7127bdbfafe49ea56a298d2758389327cab5fd
Jiwon <rkdwldnjs878@gmail.com>
2025-08-15 00:07:16 +0900

    fix: LastRouteLegCalculator null 값 필터링 추가 #360
    null 반환 가능성 대응으로 filterNotNull() 적용

--- src/main/kotlin/com/deepromeet/atcha/route/application/LastRouteLegCalculator.kt
+++ src/main/kotlin/com/deepromeet/atcha/route/application/LastRouteLegCalculator.kt
@@ -38,7 +38,7 @@ class LastRouteLegCalculator(
                         )
                     }
                 }
-            }.awaitAll()
+            }.awaitAll().filterNotNull()
         }
     }
 
