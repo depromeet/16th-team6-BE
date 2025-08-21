9028798221b03c7758a15df17a8659120caf4814
Jiwon <rkdwldnjs878@gmail.com>
2025-08-22 01:13:29 +0900

    fix: 라우트 API 컨트롤러 수정 #364

--- src/main/kotlin/com/deepromeet/atcha/route/api/RouteController.kt
+++ src/main/kotlin/com/deepromeet/atcha/route/api/RouteController.kt
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -107,10 +108,14 @@ class RouteController(
 
     @GetMapping("/user-routes/bus-arrival")
     suspend fun getBusArrivalInUserRoute(
-        @CurrentUser id: Long
-    ): ApiResponse<RealTimeBusArrivalResponse> {
+        @CurrentUser id: Long,
+        @RequestParam routeName: String
+    ): ApiResponse<List<RealTimeBusArrivalResponse>> {
         return ApiResponse.success(
-            RealTimeBusArrivalResponse(routeService.getFirstBusArrival(UserId(id)))
+            routeService.getTargetBusArrivals(
+                UserId(id),
+                routeName
+            ).map { RealTimeBusArrivalResponse(it) }
         )
     }
 
