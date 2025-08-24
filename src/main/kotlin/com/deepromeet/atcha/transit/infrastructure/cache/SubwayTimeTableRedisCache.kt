bc91acd318d0b2c80bd4a83e6b8bc8747e0fd249
Jiwon <rkdwldnjs878@gmail.com>
2025-08-24 13:39:23 +0900

    feat: 지하철 시간표 캐시 TTL 한달로 변경
    지하철 시간은 많이 조회되지만 변경은 많이 일어나지않음

--- src/main/kotlin/com/deepromeet/atcha/transit/infrastructure/cache/SubwayTimeTableRedisCache.kt
+++ src/main/kotlin/com/deepromeet/atcha/transit/infrastructure/cache/SubwayTimeTableRedisCache.kt
@@ -8,9 +8,7 @@ import com.deepromeet.atcha.transit.domain.subway.SubwayStation
 import com.deepromeet.atcha.transit.domain.subway.SubwayTimeTable
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Component
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
-import java.util.concurrent.TimeUnit
+import java.time.Duration
 
 @Component
 class SubwayTimeTableRedisCache(
@@ -35,8 +33,11 @@ class SubwayTimeTableRedisCache(
         timeTable: SubwayTimeTable
     ) {
         val key = getKey(startStation, dailyType, direction)
-        val ttlSeconds = calculateTtlUntilMidnight()
-        subwayTimeTableRedisTemplate.opsForValue().set(key, timeTable, ttlSeconds, TimeUnit.SECONDS)
+        subwayTimeTableRedisTemplate.opsForValue().set(
+            key,
+            timeTable,
+            Duration.ofDays(30)
+        )
     }
 
     private fun getKey(
@@ -45,11 +46,5 @@ class SubwayTimeTableRedisCache(
         direction: SubwayDirection
     ): String {
         return "routes:time:subway:${startStation.routeCode}-${startStation.name}:${dailyType.code}:${direction.name}"
-    }
-
-    private fun calculateTtlUntilMidnight(): Long {
-        val now = LocalDateTime.now()
-        val midnight = now.toLocalDate().plusDays(1).atStartOfDay()
-        return ChronoUnit.SECONDS.between(now, midnight)
     }
 }
