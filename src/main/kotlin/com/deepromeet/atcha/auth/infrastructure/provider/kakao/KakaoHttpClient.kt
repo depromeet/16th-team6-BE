13b3ac717936c9cfe2d05b3a0496d3432ca24dc3
Jiwon <rkdwldnjs878@gmail.com>
2025-09-11 19:55:30 +0900

    refactor: 인증 인프라를 Feign에서 HTTP Interface로 마이그레이션 #374
    - Apple, Kakao Feign 클라이언트 제거
    - HTTP Interface 기반 클라이언트로 교체

--- src/main/kotlin/com/deepromeet/atcha/auth/infrastructure/provider/kakao/KakaoHttpClient.kt
+++ src/main/kotlin/com/deepromeet/atcha/auth/infrastructure/provider/kakao/KakaoHttpClient.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 
-@HttpExchange("https://kapi.kakao.com")
+@HttpExchange
 interface KakaoHttpClient {
     @GetExchange("/v2/user/me")
     suspend fun getUserInfo(
