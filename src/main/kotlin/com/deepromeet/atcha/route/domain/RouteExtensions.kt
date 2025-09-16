92e54f5a20f94b9444fd167de8f04890864149b1
Jiwon <rkdwldnjs878@gmail.com>
2025-09-17 00:41:50 +0900

    chore: 시간이 늦어 예외 제거
    ㄹㄷ

--- src/main/kotlin/com/deepromeet/atcha/route/domain/RouteExtensions.kt
+++ src/main/kotlin/com/deepromeet/atcha/route/domain/RouteExtensions.kt
@@ -1,7 +1,5 @@
 package com.deepromeet.atcha.route.domain
 
-import com.deepromeet.atcha.route.exception.RouteError
-import com.deepromeet.atcha.route.exception.RouteException
 import java.time.LocalDateTime
 
 fun List<LastRoute>.sort(sortType: LastRouteSortType = LastRouteSortType.DEPARTURE_TIME_DESC): List<LastRoute> {
@@ -10,10 +8,6 @@ fun List<LastRoute>.sort(sortType: LastRouteSortType = LastRouteSortType.DEPARTU
         this.filter {
             it.departureDateTime.isAfter(now)
         }
-
-    if (upcomingRoutes.isEmpty()) {
-        throw RouteException.of(RouteError.NO_AVAILABLE_LAST_ROUTES)
-    }
 
     return when (sortType) {
         LastRouteSortType.MINIMUM_TRANSFERS ->
