9f502a9a8043cb86aa937e5a6c3c841a7fa17f96
Jiwon <rkdwldnjs878@gmail.com>
2025-10-04 13:47:17 +0900

    feat: 플랫폼 도메인 모델 개선 #382

--- src/main/kotlin/com/deepromeet/atcha/app/domain/Platform.kt
+++ src/main/kotlin/com/deepromeet/atcha/app/domain/Platform.kt
@@ -1,6 +1,14 @@
 package com.deepromeet.atcha.app.domain
 
-enum class Platform {
-    ANDROID,
-    IOS
+enum class Platform(val displayName: String) {
+    ANDROID("Android"),
+    IOS("iOS");
+
+    companion object {
+        fun fromString(value: String): Platform {
+            return entries.find {
+                it.displayName.equals(value, ignoreCase = true)
+            } ?: throw IllegalArgumentException("알수없는 플랫폼입니다: $value")
+        }
+    }
 }
