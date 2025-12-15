8ad8f450e3e39fb6dd2492f118abbf1b7f256190
junwon <wjdwnsdnjs13@naver.com>
2025-12-15 21:52:43 +0900

    tmp : 지하철 실시간 정보 응답

---
+++ src/main/kotlin/com/deepromeet/atcha/transit/api/response/RealTimeSubwayArrivalResponse.kt
@@ -0,0 +1,36 @@
+package com.deepromeet.atcha.transit.api.response
+
+import com.deepromeet.atcha.transit.domain.bus.BusArrival
+import com.fasterxml.jackson.annotation.JsonInclude
+import java.time.format.DateTimeFormatter
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class RealTimeSubwayArrivalResponse(
+    val routeName: String,
+//    val busStatus: SubwayStatus,
+    val remainingTime: Int,
+    val remainingStations: Int?,
+    val isLast: Boolean?,
+//    val busCongestion: BusCongestion?,
+    val remainingSeats: Int?,
+    val expectedArrivalTime: String?,
+    val vehicleId: String?
+//    val infoType: ArrivalInfoType
+) {
+    companion object {
+        private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+    }
+
+    constructor(routeName: String, busArrival: BusArrival) : this(
+        routeName,
+//        busArrival.busStatus,
+        busArrival.remainingTime,
+        busArrival.remainingStations,
+        busArrival.isLast,
+//        busArrival.busCongestion,
+        busArrival.remainingSeats,
+        busArrival.expectedArrivalTime?.format(formatter),
+        busArrival.vehicleId
+//        busArrival.infoType
+    )
+}
